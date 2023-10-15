c48b5796566ec658b7af71feef79a4372d7968ad
MourjoB <142159668+MourjoB@users.noreply.github.com>
2023-10-15 20:13:29 +0530

    Update morpher.py
    removed numpy

--- morpher.py
+++ morpher.py
@@ -1,13 +1,11 @@
 ''' 
 
 
-''' 
-import numpy as np 
-
+'''  
 #UNCOMMENT THE BELOW THREE LINES TO TEST FOR SAMPLE DATA
-#nazidims = np.array([3,2]) # mediocre
-#crossdims =  np.array([4,4]) # max dims
-#Hdims =  np.array([1,2]) # min dims  
+#nazidims = [3,2]# mediocre
+#crossdims = [4,4] # max dims
+#Hdims =  [1,2] # min dims  
 
 def paracompare(shape,paralist):
 	check = 0
@@ -21,7 +19,7 @@ def paracompare(shape,paralist):
 		return 0 #no
 
 def morph(d1,d2): 
-	dimlist = np.array([d1,d2])
+	dimlist =[d1,d2]
 	if paracompare(crossdims,dimlist) == 1:
 	    #cross()
 	    print("CROSS shape")
@@ -39,4 +37,4 @@ either debug, change drone-size or move on
 		""") 
 
 #UNCOMMENT THIS LINE TO TEST WITH SAMPLE DATA
-#morph(5,5,6)
+#morph(3,3)
