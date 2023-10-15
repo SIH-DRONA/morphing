2b721cecc10339854713f4e5bee86322f15ba644
Ayush <123345456+Spirizeon@users.noreply.github.com>
2023-10-15 18:56:22 +0530

    Create morpher.py
    Morpher function
    Inputs: Dimensions of shape
    + Will have three shapes 
    + Each shape will have predefined values
    
    Process:
    if all dimensions of each list are lower than input parameters
    named `dimlist` inside ` morph` function
    then call function
    for dimensions we are considering d1,d2 and d3
    
    
    assumption, we are considering shapes like circles as a rectangle dimensions will be called side-length, face-width, height
     
    in terms of stability, orientations are prioritised in terms of size and flight stability
    
    extra: we will be using numpy arrays for faster execution

---
+++ morpher.py
@@ -0,0 +1,42 @@
+''' 
+
+
+''' 
+import numpy as np 
+
+#UNCOMMENT THE BELOW THREE LINES TO TEST FOR SAMPLE DATA
+#nazidims = np.array([3,2,3]) # mediocre
+#crossdims =  np.array([4,4,4]) # max dims
+#Hdims =  np.array([1,2,1]) # min dims  
+
+def paracompare(shape,paralist):
+	check = 0
+	for i in range(len(paralist)):
+		if shape[i] < paralist[i]:
+			check += 1
+	print("check=",check)
+	if check == 3:
+		return 1 #yes
+	else: 
+		return 0 #no
+
+def morph(d1,d2,d3): 
+	dimlist = np.array([d1,d2,d3])
+	if paracompare(crossdims,dimlist) == 1:
+	    #cross()
+	    print("CROSS shape")
+	elif paracompare(nazidims,dimlist) == 1:
+		#nazi()  
+		print("NAZI shape")
+	elif paracompare(Hdims,dimlist) == 1: 
+		#H()
+		print("H shape")
+
+	else:
+		print("""
+ERROR: cannot determine shape
+either debug, change drone-size or move on
+		""") 
+
+#UNCOMMENT THIS LINE TO TEST WITH SAMPLE DATA
+#morph(5,5,6)
